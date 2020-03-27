d6d0262527cdbe62f620bc696f2694dc3a6838c4
sebaek <sebaek@gmail.com>
2020-03-27 16:28:11 +0900

    float double

--- src/chap02/datatype/Float1.java
+++ src/chap02/datatype/Float1.java
@@ -1,5 +1,27 @@
 package chap02.datatype;
 
 public class Float1 {
-
+	public static void main(String[] args) {
+		// 4byte 크기
+		
+		// 최대값
+		System.out.println(Float.MAX_VALUE);
+		// 최소값
+		System.out.println(Float.MIN_VALUE);
+		
+		float f;
+		f = 3.14F; // o
+		f = 3.14f; // x
+		
+		System.out.println(f);
+		
+		
+	}
 }
+
+
+
+
+
+
+

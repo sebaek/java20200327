45903f89474ba04a79c37d52db5cde3e5204e35c
sebaek <sebaek@gmail.com>
2020-03-27 16:14:44 +0900

    double

---
+++ src/chap02/datatype/Double1.java
@@ -0,0 +1,26 @@
+package chap02.datatype;
+
+public class Double1 {
+	public static void main(String[] args) {
+		// 8byte 크기
+		// 부동소수점 (floating point) (ieee 754)
+		
+		// 최대값(1.7976931348623157E308) == (1.79 * 10^308)
+		System.out.println(Double.MAX_VALUE);
+		// 최소값(4.9E-324) == (4.9 * 10^-324)
+		System.out.println(Double.MIN_VALUE);
+		
+		double d = 3.14;
+		d = 3.14E-2;
+		System.out.println(d);
+		
+		
+		
+	}
+}
+
+
+
+
+
+
---
+++ src/chap02/datatype/Float1.java
@@ -0,0 +1,5 @@
+package chap02.datatype;
+
+public class Float1 {
+
+}

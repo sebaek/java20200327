836d43977489e2e99f2e09178ae51354d3fb57cf
sebaek <sebaek@gmail.com>
2020-03-27 10:24:36 +0900

    test

--- src/myjava/Hello.java
+++ src/myjava/Hello.java
@@ -2,7 +2,9 @@ package myjava;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("hello java");  
+		System.out.println("hello java");
+		
+		// 수정해서 올린 파일
 	}
 
 }

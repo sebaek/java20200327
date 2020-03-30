3389d878704abd099b560fa00178b597fb764c41
sebaek <sebaek@gmail.com>
2020-03-30 09:37:49 +0900

    type casting

---
+++ src/chap02/typecasting/Explitcit.java
@@ -0,0 +1,16 @@
+package chap02.typecasting;
+
+public class Explitcit {
+	public static void main(String[] args) {
+		// byte → short → int → long → float → double
+		//         char ↗
+		
+		int i = 103029770;
+		byte b = (byte) i;
+		System.out.println(i);
+		System.out.println(b);
+		
+		
+	}
+
+}

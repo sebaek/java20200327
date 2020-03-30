277680e887c860541dcb55cf7ad5f90d030c04b0
sebaek <sebaek@gmail.com>
2020-03-30 15:37:45 +0900

    logical not

---
+++ src/chap03/unary/LogicalNot.java
@@ -0,0 +1,26 @@
+package chap03.unary;
+
+public class LogicalNot {
+	public static void main(String[] args) {
+		boolean a = true;
+		
+		boolean b = !a;
+		System.out.println(b);
+		System.out.println(a);
+		
+		
+		boolean c = false;
+		b = !c;
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
+
+
+
+
+
+
+
+
+

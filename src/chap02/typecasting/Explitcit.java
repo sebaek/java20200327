bfd666104667116ea31d5931200cd6751b475635
sebaek <sebaek@gmail.com>
2020-03-30 10:10:07 +0900

    type casting

--- src/chap02/typecasting/Explitcit.java
+++ src/chap02/typecasting/Explitcit.java
@@ -10,7 +10,32 @@ public class Explitcit {
 		System.out.println(i);
 		System.out.println(b);
 		
+		long l = 300;
+		i = (int) l;
+		System.out.println(l);
+		System.out.println(i);
+		
+		int intValue = 'A';
+		char charValue = (char) intValue;
+		System.out.println(charValue);
+		
+		double d = 3.14;
+		int i3 = (int) d;
+		System.out.println(i3);
+		
+		
+		
+		
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+

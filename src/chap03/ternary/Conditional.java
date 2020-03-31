0948b4e5b4edd3f3608e845f92e222c206a746a5
sebaek <sebaek@gmail.com>
2020-03-31 16:23:05 +0900

    ternary

---
+++ src/chap03/ternary/Conditional.java
@@ -0,0 +1,24 @@
+package chap03.ternary;
+
+public class Conditional {
+	public static void main(String[] args) {
+		// 1항 ? 2항 : 3항 → 2항 또는 3항
+		
+		// 1항이 true  이면 2항
+		// 1항이 false 이면 3항
+		
+		int i = (true ? 3 : 4);
+		System.out.println(i);
+		i = (false ? 3 : 4);
+		System.out.println(i);
+		
+		
+		
+	}
+}
+
+
+
+
+
+

838b951105b856f856d4ab70da4990cde32e4c4d
sebaek <sebaek@gmail.com>
2020-04-02 11:46:13 +0900

    continue

--- src/chap04/breakContinue/BreakContinue.java
+++ src/chap04/breakContinue/BreakContinue.java
@@ -2,15 +2,43 @@ package chap04.breakContinue;
 
 public class BreakContinue {
 	public static void main(String[] args) {
-
-		outter: for (int j = 0; j < 5; j++) {
+		// break;
+		for (int j = 0; j < 5; j++) {
 			System.out.println("j: " + j);
 			for (int i = 0; i < 5; i++) {
 				System.out.println("i: " + i);
 				if (i == 2) {
-					break outter;
+					break;
 				}
 			}
 		}
+		
+
+		// continue;
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+			if (i == 2) {
+				continue;
+			}
+			System.out.println("continue 이후 실행문");
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

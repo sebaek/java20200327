4ad79d7409052b50aeb77b8f04f2a631250c33f0
sebaek <sebaek@gmail.com>
2020-04-03 09:44:36 +0900

    기본타입과 참조타입

---
+++ src/chap05/reftype/RefType.java
@@ -0,0 +1,24 @@
+package chap05.reftype;
+
+public class RefType {
+	public static void main(String[] args) {
+		int a = 25;
+		double d = 3.14;
+		
+		String s = "java";
+		int[] arr = {1, 2};
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+

69832bead79fbc7c4e56bd1c0ce5ffdf89058ca4
sebaek <sebaek@gmail.com>
2020-04-03 12:36:20 +0900

    2d to 1d

--- src/chap05/array/multi/Problem1.java
+++ src/chap05/array/multi/Problem1.java
@@ -2,17 +2,27 @@ package chap05.array.multi;
 
 public class Problem1 {
 	public static void main(String[] args) {
-		int[][] origin = {{3,4,5}, {100, 101}, {-3, -2, -1, 0}};
+		int[][] origin = { { 3, 4, 5 }, { 100, 101 }, { -3, -2, -1, 0 } };
 		int[] target = null;
-		//{0, -1, -2, -3, 101, 100, 5, 4, 3};
+		// {0, -1, -2, -3, 101, 100, 5, 4, 3};
+		int len = 0;
+		for (int i = 0; i < origin.length; i++) {
+//			System.out.println(origin[i].length);
+			len += origin[i].length;
+		}
+		target = new int[len];
 		
+		int k = 0;
+		for (int i = origin.length - 1; i >= 0; i--) {
+			for (int j = origin[i].length - 1; j >= 0; j--) {
+				target[k] = origin[i][j];
+				k++;
+			}
+		}
+		System.out.println();
 		for (int i = 0; i < target.length; i++) {
 			System.out.println(target[i]);
 		}
-		
-		
-		
-		
-		
+
 	}
 }

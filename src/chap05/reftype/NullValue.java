e589258bd1e049894c4803e6a410994c2842af3c
sebaek <sebaek@gmail.com>
2020-04-03 10:38:04 +0900

    null

---
+++ src/chap05/reftype/NullValue.java
@@ -0,0 +1,29 @@
+package chap05.reftype;
+
+public class NullValue {
+	public static void main(String[] args) {
+		// 그림: 04. 널값
+		String s1;
+		s1 = null;
+		
+		String s2 = "java";
+		s2 = null;
+		
+		System.out.println(s2 == null);
+		System.out.println(s2 != null);
+		
+		if (s2 != null) {
+			System.out.println(s2.length());
+		}
+		
+		if (s1 != null) {
+			System.out.println(s1.length());
+		}
+		
+	}
+}
+
+
+
+
+

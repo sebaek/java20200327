ce09f8be1007d94abe395f31eb5174ee973777d4
sebaek <sebaek@gmail.com>
2020-04-07 11:50:51 +0900

    sample

--- src/chap06/lecture/sample/StudentApp.java
+++ src/chap06/lecture/sample/StudentApp.java
@@ -11,11 +11,9 @@ public class StudentApp {
 
 		boolean run = true;
 		while (run) {
-			System.out.println(
-					"----------------------------");
+			System.out.println("----------------------------");
 			System.out.println("1.학생추가 | 2.통계 | 3.종료");
-			System.out.println(
-					"----------------------------");
+			System.out.println("----------------------------");
 			System.out.print("선택>");
 			String menu = scanner.nextLine();
 
@@ -24,28 +22,25 @@ public class StudentApp {
 				System.out.print("이름>");
 				String name = scanner.nextLine();
 				System.out.print("국어>");
-				int korean = Integer
-						.parseInt(scanner.nextLine());
+				int korean = Integer.parseInt(scanner.nextLine());
 				System.out.print("영어>");
 				int english = Integer
 						.parseInt(scanner.nextLine());
 				System.out.print("수학>");
-				int math = Integer
-						.parseInt(scanner.nextLine());
+				int math = Integer.parseInt(scanner.nextLine());
 				students[size] = new Student(name, korean,
 						english, math);
 				size++;
 				break;
 			case "2":
-				System.out
-						.println("이름\t국어\t영어\t수학\t최고\t평균");
+				System.out.println("이름\t국어\t영어\t수학\t최고\t평균");
 				for (int i = 0; i < size; i++) {
-					System.out.println(students[i].name
-							+ "\t" + students[i].korean
-							+ "\t" + students[i].english
-							+ "\t" + students[i].math + "\t"
-							+ students[i].getMaxScore()
-							+ "\t" + students[i].getAvg());
+					System.out.println(students[i].name + "\t"
+							+ students[i].korean + "\t"
+							+ students[i].english + "\t"
+							+ students[i].math + "\t"
+							+ students[i].getMaxScore() + "\t"
+							+ students[i].getAvg());
 				}
 				break;
 

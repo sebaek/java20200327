9338427a632c56d5ab71ab0d50126ff1b4df7e1b
sebaek <sebaek@gmail.com>
2020-04-08 17:43:51 +0900

    warmup1

--- src/codingbat/WarmUP1.java
+++ src/codingbat/WarmUP1.java
@@ -234,4 +234,14 @@ public class WarmUP1 {
 
 	}
 
+	public String everyNth(String str, int n) {
+		String r = "";
+
+		for (int i = 0; i < str.length(); i += n) {
+			r += str.charAt(i);
+		}
+
+		return r;
+	}
+
 }

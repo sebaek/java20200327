368d545dd0c5eb4a085a7d2a1e5dd7dda2e7eb09
sebaek <sebaek@gmail.com>
2020-04-08 15:09:16 +0900

    exercise

--- src/chap06/exercises/ex20/BankApplication.java
+++ src/chap06/exercises/ex20/BankApplication.java
@@ -34,6 +34,46 @@ public class BankApplication {
 				run = false;
 			}
 		}
+		System.out.println("프로그램 종료");
+	}
+
+	private static void withdraw() {
+		System.out.println("----------------");
+		System.out.println("출금");
+		System.out.println("----------------");
+		System.out.print("계좌번호:");
+		String ano = scanner.nextLine();
+		Account account = findAccount(ano);
+		System.out.print("출금액:");
+		int money = scanner.nextInt();
+		scanner.nextLine();
+		account.setBalance(account.getBalance() - money);
+		System.out.println("결과: 출금이 성공되었습니다.");
+		
+	}
+
+	private static void deposit() {
+		System.out.println("----------------");
+		System.out.println("예금");
+		System.out.println("----------------");
+		System.out.print("계좌번호:");
+		String ano = scanner.nextLine();
+		Account account = findAccount(ano);
+		System.out.print("예금액:");
+		int money = scanner.nextInt();
+		scanner.nextLine();
+		account.setBalance(account.getBalance() + money);
+		System.out.println("결과: 예금이 성공되었습니다.");
+		
+	}
+	
+	private static Account findAccount(String ano) {
+		for (int i = 0; i < size; i++) {
+			if (accountArray[i].getAno().equals(ano)) {
+				return accountArray[i];
+			}
+		}
+		return null;
 	}
 
 	private static void createAcount() {

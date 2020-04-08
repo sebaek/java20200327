fee535de7616a6f1e070da6d554245bcc1eda990
sebaek <sebaek@gmail.com>
2020-04-08 12:01:54 +0900

    chap06

---
+++ src/chap06/examples/sec061301/package2/C.java
@@ -0,0 +1,17 @@
+package chap06.examples.sec061301.package2;
+
+//import chap06.examples.sec061301.package1.A;
+import chap06.examples.sec061301.package1.B;
+
+public class C {
+//	A a;
+	B b;
+}
+
+class D {
+	
+}
+
+class E {
+	
+}
---
+++ src/chap06/lecture/encapsulation/Car.java
@@ -0,0 +1,34 @@
+package chap06.lecture.encapsulation;
+
+public class Car {
+	private int speed;
+	private String airbag;
+	
+	public Car() {
+		this.airbag = "전면";
+	}
+	
+	public String getAirbag() {
+		return this.airbag;
+	}
+	
+	public void setSpeed(int speed) {
+		if (speed < 0) {
+			this.speed = 0;
+		} else {
+			this.speed = speed;
+		}
+	}
+	
+	public int getSpeed() {
+		return this.speed;
+	}
+}
+
+
+
+
+
+
+
+

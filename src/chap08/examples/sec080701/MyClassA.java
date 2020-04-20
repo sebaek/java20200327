691e87a572be8f35d01b2d9002c8ae5852ec998f
sebaek <sebaek@gmail.com>
2020-04-20 16:34:46 +0900

    default

---
+++ src/chap08/examples/sec080701/MyClassA.java
@@ -0,0 +1,8 @@
+package chap08.examples.sec080701;
+
+public class MyClassA implements MyInterface {
+	@Override
+	public void method1() {
+		System.out.println("MyClassA-method1() 실행");
+	}
+}
---
+++ src/chap08/examples/sec080701/MyInterface.java
@@ -0,0 +1,8 @@
+package chap08.examples.sec080701;
+
+public interface MyInterface {
+	public void method1();
+	public default void method2() {
+		System.out.println("MyInterface-method2 실행");
+	}
+}

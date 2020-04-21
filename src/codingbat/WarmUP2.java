a6f0e892881e50f094ab1869735352654c6ce249
sebaek <sebaek@gmail.com>
2020-04-21 12:27:03 +0900

    warmup2

--- src/codingbat/WarmUP2.java
+++ src/codingbat/WarmUP2.java
@@ -184,4 +184,17 @@ public class WarmUP2 {
 		return str.replace("yak", "");
 	}
 
+	public int array667(int[] nums) {
+		int cnt = 0;
+
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == 6) {
+				if (nums[i + 1] == 6 || nums[i + 1] == 7) {
+					cnt++;
+				}
+			}
+		}
+
+		return cnt;
+	}
 }
